fa029807ae7a9c9deae79e84ed26b07047d7f295
이호현 <hohyeon@Freddies-mac-mini.local>
2022-10-20 09:10:25 +0900

    feat : add cors config

--- src/main/java/com/backend/sketchbrain/sketchbrainbackend/SketchBrainBackendApplication.java
+++ src/main/java/com/backend/sketchbrain/sketchbrainbackend/SketchBrainBackendApplication.java
@@ -2,6 +2,9 @@ package com.backend.sketchbrain.sketchbrainbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SketchBrainBackendApplication {
@@ -10,4 +13,15 @@ public class SketchBrainBackendApplication {
         SpringApplication.run(SketchBrainBackendApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+                registry.addMapping("/**").allowedOrigins("http://localhost:8080");
+                registry.addMapping("/**").allowedOrigins("http://203.252.166.213:30801");
+            }
+        };
+    }
 }

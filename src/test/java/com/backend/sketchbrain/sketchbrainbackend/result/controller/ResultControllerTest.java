b687f7c5a7adc60b3bb35478123eb4050c1bca85
이호현 <hhlee@agilesoda.ai>
2022-10-27 20:41:33 +0900

    feat : add result controller test code

--- src/test/java/com/backend/sketchbrain/sketchbrainbackend/result/controller/ResultControllerTest.java
+++ src/test/java/com/backend/sketchbrain/sketchbrainbackend/result/controller/ResultControllerTest.java
@@ -1,5 +1,6 @@
 package com.backend.sketchbrain.sketchbrainbackend.result.controller;
 
+import com.backend.sketchbrain.sketchbrainbackend.global.error.exceptions.ResultErrorCodeImpl;
 import com.backend.sketchbrain.sketchbrainbackend.result.dto.Result;
 import com.backend.sketchbrain.sketchbrainbackend.result.service.ResultService;
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -15,6 +16,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -22,6 +24,7 @@ import java.util.concurrent.ConcurrentHashMap;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -44,8 +47,7 @@ class ResultControllerTest {
                 new Result(2,"user2","data.csv","model.py","21",null),
                 new Result(3,"user3","data.csv","model.py","21",null)
         });
-        Map<String, Object> result = new ConcurrentHashMap<>();
-        result.put("result",allResultList);
+
         given(resultService.getAllResultList()).willReturn(allResultList);
 
         mvc.perform(get("/api/server/result"))
@@ -56,14 +58,76 @@ class ResultControllerTest {
     }
 
     @Test
-    void insertResult() {
+    @DisplayName("Result 를 추가하는 Controller 로직이 정상 작동한다.")
+    void insertResult() throws Exception {
+        Result insertResult = new Result("user","data.csv","model.py","23");
+        mvc.perform(put("/api/server/result"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("success").value(true))
+                .andReturn();
     }
 
     @Test
-    void resultListByUser() {
+    @DisplayName("정상적이지 않은 Result 는 추가되지 않고, 에러를 반환한다")
+    void insertIncorrectResult() throws Exception{
+        Result insertIncorrectResult = new Result();
+        insertIncorrectResult.setId(1);
+        insertIncorrectResult.setUser("incorrect user");
+
+        mvc.perform(put("/api/server/result"))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("message").value(ResultErrorCodeImpl.EMPTY_PARAMETER_EXIST.getMessage()))
+                .andExpect(jsonPath("error").isNotEmpty())
+                .andReturn();
     }
 
     @Test
-    void resultListById() {
+    @DisplayName("User 이름을 통한 Result List 를 반환하는 Controller 로직이 정상 작동한다.")
+    void resultListByUser() throws Exception {
+        List<Result> testResultListByUser =  List.of(new Result[]{
+                new Result("user1","data1.csv","model1.csv","21"),
+                new Result("user1","data2.csv","model2.csv","22")
+        });
+        given(resultService.getResultListByUser("user1")).willReturn(testResultListByUser);
+
+        mvc.perform(get("/api/server/result/user/{user}","user1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("result").isNotEmpty());
+    }
+
+    @Test
+    @DisplayName("User 이름이 DB에 없으면, Not Found 를 반환한다.")
+    void resultListByIncorrectUser() throws  Exception{
+        String userName = "@incorrect! user^ &name*";
+        List<Result> testResultListByIncorrectUser = new ArrayList<>();
+        given(resultService.getResultListByUser(userName)).willReturn(testResultListByIncorrectUser);
+
+        mvc.perform(get("/api/server/result/user/{user}",userName))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("message").value(ResultErrorCodeImpl.UKNOWN_USER_REFERED.getMessage()));
+    }
+    @Test
+    @DisplayName("id 를 통한 Result List 를 반환하는 Controller 로직이 정상 작동한다.")
+    void resultListById() throws Exception {
+        List<Result> testResultListById =  List.of(new Result[]{
+                new Result(1,"user1","data1.csv","model1.csv","21",null)
+        });
+        given(resultService.getResultListById("1")).willReturn(testResultListById);
+
+        mvc.perform(get("/api/server/result/id/{id}","1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("result").isNotEmpty());
+    }
+
+    @Test
+    @DisplayName("id 가 DB에 없으면, Not Found 를 반환한다.")
+    void resultLlistByIncorrectId() throws Exception{
+        List<Result> testResultListByIncorrectId = new ArrayList<>();
+        String id = "@incorrect! id^";
+        given(resultService.getResultListById(id)).willReturn(testResultListByIncorrectId);
+
+        mvc.perform(get("/api/server/result/id/{id}",id))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("message").value(ResultErrorCodeImpl.UKNOWN_ID_REFERED.getMessage()));
     }
 }
